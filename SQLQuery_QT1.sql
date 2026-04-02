d287fcf2e884b257def94c98c048cb4a7ca31f6a
Quoc Trung <doquoctrung2k@gmail.com>
2026-04-02 14:01:06 +0700

    add sql và sql 2019

---
+++ SQLQuery_QT1.sql
@@ -0,0 +1,28 @@
+﻿
+CREATE DATABASE SmartTicketingDB;
+GO
+
+USE SmartTicketingDB;
+GO
+
+
+CREATE TABLE Routes (
+    RouteID INT IDENTITY(1,1) PRIMARY KEY,
+    RouteName NVARCHAR(100),
+    Price INT
+);
+
+
+CREATE TABLE Tickets (
+    TicketID INT IDENTITY(1,1) PRIMARY KEY,
+    RouteID INT FOREIGN KEY REFERENCES Routes(RouteID),
+    IssueDate DATETIME,
+    PaymentMethod NVARCHAR(50),
+    Status NVARCHAR(50)
+);
+
+
+INSERT INTO Routes (RouteName, Price) VALUES 
+(N'Bến Thành - Suối Tiên', 15000),
+(N'Bến Thành - An Phú', 12000),
+(N'Bến Thành - Thảo Điền', 10000);
